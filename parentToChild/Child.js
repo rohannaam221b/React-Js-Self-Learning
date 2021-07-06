2007b1ce383fda2c895abc8202d678451b751ba2
rohannaam221b <83057789+rohannaam221b@users.noreply.github.com>
2021-07-06 18:52:28 +0530

    Add files via upload

---
+++ parentToChild/Child.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Child = (props) => {
+  return (
+    <div>
+      <button onClick={props.doWhatever}>{props.title}</button>
+    </div>
+  )
+}
+
+export default Child
